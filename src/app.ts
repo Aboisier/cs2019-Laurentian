264610ab1b38221b150d2c6cf91dd3a27e047d97
ChellSki <40728505+ChellSki@users.noreply.github.com>
2019-03-23 15:08:27 -0400

    New changes
    create account

--- src/app.ts
+++ src/app.ts
@@ -25,13 +25,35 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
+// Liveness
+//POST Status = up upon routing to api/status
+const api = new Api(); // create new api
+app.use('/api', api.getRouter()); // use current api on route /api
 
-const api = new Api();
-app.use('/api', api.getRouter());
-
+// upon a get request to the route /api/status, return the status of the server = "up"
 app.get( "/api/status", ( req: any, res ) => {
     res.json({status: "up"});
  });
+
+// Create Account
+app.post( "/api/auth/createAccount", ( req: any, res ) => {
+    // if all fields have been entered
+    if (fullName != null && email !=null && password != null)
+        req.json({status_code: "201", creation_status: "Creation Sucessful"});
+    
+    // if either fields have not been entered
+    if (fullName != null || email !=null || password != null)
+        req.json({status_code: "400", creation_status: "Creation Unsucessful"});
+
+    // if email is already in use
+    if (femail.isAlreadyStoredInDatabase() == true)
+        req.json({status_code: "500", creation_status: "Creation Unsucessful"});
+});
+
+
+
+
+
 
 if (isDev()) {
     app.use(express.static(path.resolve(__dirname, '../frontend')));
